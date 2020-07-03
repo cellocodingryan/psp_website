79fd277eca0114beac213576089b7c57f66a5b54
cellocodingryan <cellocodingryan@gmail.com>
2020-07-03 18:42:58 -0400

    added add attr to response

--- models/response.php
+++ models/response.php
@@ -7,11 +7,14 @@ class response {
         $this->message = $message;
         $this->status = $status;
     }
+    public function add_attr($name,$val) {
+        $this->vals[$name] = $val;
+    }
     public function __toString()
     {
-        return json_encode(["status"=>$this->status,"message"=>$this->message]);
+        return json_encode(array_merge(["status"=>$this->status,"message"=>$this->message],$this->vals));
     }
-
+    private $vals = [];
     private $status;
     private $message;
 }
